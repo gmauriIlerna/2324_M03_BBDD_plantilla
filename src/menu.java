0e000a4fc0e7e59ad8787e720883fae23c6e4199
gmauriIlerna <gmauri@ilerna.com>
2024-01-18 16:21:25 +0100

    Example added

--- src/menu.java
+++ src/menu.java
@@ -4,8 +4,29 @@ public class menu {
 	public static Connection con;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Ejemplo de uso con la tabla ACTOR y las columnas NACTOR NOMBRE FECHAN
+		Connection con = bbdd.conectarBaseDatos();
+		String[] a = {"NACTOR", "NOMBRE", "FECHAN"};
+		bbdd.print(con, "SELECT * FROM ACTOR", a);
+		System.out.println("Insert");
+		bbdd.insert(con, "INSERT INTO ACTOR (\"NACTOR\", \"NOMBRE\", \"FECHAN\")\n"
+				+ "VALUES (2, 'John Doe', TO_DATE('2024-01-18', 'YYYY-MM-DD'))");
+		bbdd.print(con, "SELECT * FROM ACTOR", a);
+		System.out.println("Update");
+		bbdd.update(con, "UPDATE ACTOR\n"
+				+ "SET \"NOMBRE\" = 'New Name'\n"
+				+ "WHERE \"NACTOR\" = 2 ");
+		bbdd.print(con, "SELECT * FROM ACTOR", a);
+		System.out.println("Delete");
+		bbdd.delete(con, "DELETE FROM ACTOR\n"
+				+ "WHERE \"NACTOR\" = 2");
+		bbdd.print(con, "SELECT * FROM ACTOR", a);
+		System.out.println("Select");
+		String[] b = bbdd.select(con, "SELECT * FROM ACTOR", a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("Columna " + a[i] + " resultado " + b[i]);
+		}
 	}
 
 }
